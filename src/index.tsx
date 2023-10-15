db43428234a08644e9b2c24c8789ac47a258c106
hagelt18 <hagelt18@gmail.com>
2023-10-15 10:41:23 -0500

    Update react-dom render

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import AppRouter from './router/AppRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-ReactDOM.render(
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(
   <Provider store={store}>
     <AppRouter />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
